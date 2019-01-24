3537f831e938d9e8615356369667fb843fc106dd
aitor <aitor@e451.net>
2019-01-24 15:46:26 +0100

    video logo present

--- web/app/themes/mrg/resources/assets/scripts/routes/home.js
+++ web/app/themes/mrg/resources/assets/scripts/routes/home.js
@@ -1,6 +1,20 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
+
+    let logo = $('.logo');
+    let ruido =$('.ruido');
+    let video =$('.video');
+
+    $('.menu-present').mouseenter(function(){
+      ruido.css('opacity', 0.3);
+      video.css('background-image', 'url("/app/themes/mrg/dist/images/videogif/present.gif")')
+    });
+
+    $('.menu-item').mouseleave(function(){
+      ruido.css('opacity', 1);
+      video.css('background-image', 'none')
+    });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
