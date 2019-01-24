85fdc1aa2d9ffc7e70d36fe3f7ac2af68b522289
aitor <aitor@e451.net>
2019-01-24 19:32:49 +0100

    brownian motion menu items on hover

--- web/app/themes/mrg/resources/assets/scripts/routes/home.js
+++ web/app/themes/mrg/resources/assets/scripts/routes/home.js
@@ -17,9 +17,23 @@ export default {
       ruido.css('opacity', 1);
       video.css('background-image', 'none')
     });
+
+
+
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
+
+    // movimiento browniano menús
+    let viewportWidth = $(window).width();
+
+    if (viewportWidth >= 700) {
+      $('.menu-item').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', function(e) {
+        $(this).css({top: Math.floor(Math.random() * 3) + 'px'},0);
+        $(this).css({left: Math.floor(Math.random() * 3) + 'px'},0);
+      });
+    }
+
   },
 };
 /* eslint-enable */
