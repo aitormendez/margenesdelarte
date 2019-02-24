df17116b003d355f54cdf3143cec0f21713fba3f
aitor <aitor@e451.net>
2019-02-24 18:43:42 +0100

    updates

---
+++ web/app/themes/mrg/resources/views/partials/footer-bak.blade.php
@@ -0,0 +1,38 @@
+<footer class="content-info">
+  <div class="container">
+    <div>@if (has_nav_menu('general_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'general_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+      @if (has_nav_menu('etc_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'etc_navigation', 'menu_class' => 'nav']) !!}
+      @endif</div>
+    @if (has_nav_menu('contents_footer_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'contents_footer_navigation', 'menu_class' => 'nav']) !!}
+    @endif
+    @if (has_nav_menu('activities_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'activities_navigation', 'menu_class' => 'nav']) !!}
+    @endif
+    <div class="social">
+      <!-- Facebook icon -->
+      <a href="https://www.facebook.com/margenesdelarte/" target="_blank">
+        <i class="fab fa-facebook"></i>
+      </a>
+
+      <!-- Twitter icon -->
+      <a href="#">
+        <i href="https://twitter.com/margenesdelarte" target="_blank" class="fab fa-twitter"></i>
+      </a>
+    </div>
+    <div class="logos">
+      <div class="logo-aguila">
+        @svg('logo-aguila')
+      </div>
+      <div class="logo-cam">
+        @svg('logo-cam-negativo')
+      </div>
+    </div>
+
+
+    @php dynamic_sidebar('sidebar-footer') @endphp
+  </div>
+</footer>
